88acdd279695dac2bfaa3f5a9b622e77badeea47
Arthur Zopellaro <arthurazsoares@gmail.com>
2018-12-11 18:33:59 -0200

    Update iperf

--- topo_iperf.py
+++ topo_iperf.py
@@ -42,8 +42,8 @@ def main():
     mn = Mininet(topo=topo, controller=RemoteController)
     mn.start()
     h2 = mn.get('h2')
-    h2.cmdPrint('iperf -s -u -i 1 -p 11111 > logs/iperf1.log 2>&1 &')
-    h2.cmdPrint('iperf -s -u -i 1 -p 22222 > logs/iperf2.log 2>&1 &')
+    # h2.cmdPrint('iperf -s -u -i 1 -p 11111 > logs/iperf1.log 2>&1 &')
+    # h2.cmdPrint('iperf -s -u -i 1 -p 22222 > logs/iperf2.log 2>&1 &')
     CLI(mn)
     mn.stop()
 
