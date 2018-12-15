a4eeac7e3abc8471c7e4ab1d122f6cf5992e908d
Arthur Zopellaro <arthurazsoares@gmail.com>
2018-12-15 21:19:15 -0200

    Apply conventions

--- exerc_dijkstra.py
+++ exerc_dijkstra.py
@@ -1,13 +1,13 @@
 # Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 # implied.
 # See the License for the specific language governing permissions and
@@ -23,12 +23,8 @@ from ryu.ofproto import ether
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
-from ryu.lib.packet import tcp
-from ryu.lib.packet import ipv4
-from ryu.lib.packet import arp
 from ryu.controller import dpset
 from ryu.topology import event
-from ryu.lib import dpid as dpid_lib
 from ryu.lib import stplib
 from ryu.lib import mac
 
@@ -41,14 +37,16 @@ class enlace:
         self.dst_port = dst_port
 
     def __str__(self):
-        return '{}:{} -> {}:{}'.format(str(self.dpid_src), str(self.src_port), str(self.dpid_dst), str(self.dst_port))
+        return ('{}:{} -> {}:{}'.format(
+            str(self.dpid_src), str(self.src_port),
+            str(self.dpid_dst), str(self.dst_port)))
 
 
 class topologia:
     def __init__(self):
         self.sws = []
         self.dpid_to_mac = {}
-        #self.dpid_to_mac.setdefault(0, [])
+        self.dpid_to_mac.setdefault(0, [])
         self.enlaces = []
         self.dpid_hosts = {}
 
@@ -71,7 +69,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         }
         self.dpset = kwargs.get('dpset', None)
         self.stplib = kwargs.get('stplib', None)
-    	self.topo = topologia()
+        self.topo = topologia()
         self.first_time = True
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -121,33 +119,36 @@ class SimpleSwitch13(app_manager.RyuApp):
     @set_ev_cls(event.EventSwitchEnter, MAIN_DISPATCHER)
     def entrou_switch(self, ev):
         # list: ev.switch.ports
-        # {'hw_addr': '1a:a5:c5:a3:c5:16', '_config': 0, 'name': 's1-eth1', '_state': 4, 'dpid': 1, 'port_no': 1}
-        print ("Switch ID: {} entrou na topologia!".format(ev.switch.dp.id))
+        # {'hw_addr': '1a:a5:c5:a3:c5:16', '_config': 0, 'name': 's1-eth1',
+        # '_state': 4, 'dpid': 1, 'port_no': 1}
+        print('Switch ID: {} entrou na topologia!'.format(ev.switch.dp.id))
         for port in ev.switch.ports:
-            print '>>>>>>>', port.__dict__
-            #self.topo.dpid_to_mac.setdefault(ev.switch.dp.id, [])
-            #self.topo.dpid_to_mac[ev.switch.dp.id].append(port.hw_addr)
+            print('>>>>>>>', port.__dict__)
+            # self.topo.dpid_to_mac.setdefault(ev.switch.dp.id, [])
+            # self.topo.dpid_to_mac[ev.switch.dp.id].append(port.hw_addr)
             self.topo.dpid_to_mac.setdefault(ev.switch.dp.id, {})
-            self.topo.dpid_to_mac[ev.switch.dp.id].setdefault(port.port_no,port.hw_addr)
-        print '\n'
+            self.topo.dpid_to_mac[ev.switch.dp.id].setdefault(
+                port.port_no, port.hw_addr)
+        print('\n')
         self.topo.sws.append(ev.switch.dp.id)
-
 
     @set_ev_cls(event.EventSwitchLeave, MAIN_DISPATCHER)
     def saiu_switch(self, ev):
-        print "Switch ID: {} saiu da topologia!".format(ev.switch.dp.id)
+        print('Switch ID: {} saiu da topologia!'.format(ev.switch.dp.id))
         del self.topo.sws[self.topo.sws.index(ev.switch.dp.id)]
 
     # LINK
     @set_ev_cls(event.EventLinkAdd, MAIN_DISPATCHER)
     def entrou_link(self, ev):
-        print "ENTROU um link!"  # {} <> {}".format(ev.link.src.__dict__, ev.link.dst.__dict__)
+        print('ENTROU um link!')
+        # {} <> {}'.format(ev.link.src.__dict__, ev.link.dst.__dict__)
         self.topo.enlaces.append(enlace(
-            ev.link.src.dpid, ev.link.src.port_no, ev.link.dst.dpid, ev.link.dst.port_no))
+            ev.link.src.dpid, ev.link.src.port_no,
+            ev.link.dst.dpid, ev.link.dst.port_no))
 
     @set_ev_cls(event.EventLinkDelete, MAIN_DISPATCHER)
     def saiu_link(self, ev):
-        print "SAIU um link!"
+        print('SAIU um link!')
         # del self.topo.elnaces[self.topo.enlaces.index(ev.switch.dp.id)]
 
     # -------------------------------------------------------------------------------
@@ -174,33 +175,38 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         dpid = datapath.id
 
-        print "PACKET_IN! DPID:",dpid
+        print('PACKET_IN! DPID:', dpid)
 
         mac_is_of_switch = False
-        #print ">>>SWITCHES MACS (DPID,PORT_NO,MAC)",self.topo.dpid_to_mac
+        # print('>>>SWITCHES MACS (DPID,PORT_NO,MAC)',self.topo.dpid_to_mac)
 
-        for switch_port_list in self.topo.dpid_to_mac.values(): # Check if the source mac is from a switch
+        for switch_port_list in self.topo.dpid_to_mac.values():
+            # Check if the source mac is from a switch
             if src in switch_port_list.values():
                 mac_is_of_switch = True
 
-        if src not in self.topo.dpid_hosts.values() and not mac_is_of_switch: # Save only macs that are not from switches
+        if src not in self.topo.dpid_hosts.values() and not mac_is_of_switch:
+            # Save only macs that are not from switches
             self.topo.dpid_hosts[dpid] = src
 
-        #print "HOSTS_LIST >>",self.topo.dpid_hosts
+        # print('HOSTS_LIST >>',self.topo.dpid_hosts)
 
-        #print self.topo
-        #if self.first_time:
-        if dst in self.topo.dpid_hosts.values(): # Check if the host location is known
-            print "---MESSAGE---\nFROM", dpid
+        # print self.topo
+        # if self.first_time:
+        if dst in self.topo.dpid_hosts.values():
+            # Check if the host location is known
+            print('---MESSAGE---\nFROM', dpid)
             dpid_paths = self.dijkstra(self.topo, dpid)
-            for switch_id, host_mac in self.topo.dpid_hosts.items():    # Get the switch id which is connected to the destination host
+            for switch_id, host_mac in self.topo.dpid_hosts.items():
+                # Get the switch id which is connected to the destination host
                 if host_mac == dst:
                     dest_dpid = switch_id
-                    print ">>> HOST ESTA CONECTADO AO SWITCH:", dest_dpid
-            print "TO", dest_dpid
-            print "PATHS",dpid_paths
-            out_port = self.get_out_port(dpid, dest_dpid, dpid_paths, self.topo.enlaces)
-            print "OUTPORT RESULT",out_port
+                    print('>>> HOST ESTA CONECTADO AO SWITCH:', dest_dpid)
+            print('TO', dest_dpid)
+            print('PATHS', dpid_paths)
+            out_port = self.get_out_port(
+                dpid, dest_dpid, dpid_paths, self.topo.enlaces)
+            print('OUTPORT RESULT', out_port)
         else:
             out_port = ofproto.OFPP_FLOOD
 
@@ -208,15 +214,20 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-
-            #actions = [parser.OFPActionSetField(eth_src=self.topo.dpid_to_mac[dpid][out_port]), parser.OFPActionDecNwTtl(), parser.OFPActionOutput(out_port)]
-            actions = [parser.OFPActionDecNwTtl(), parser.OFPActionOutput(out_port)]
+            # actions = [
+            #     parser.OFPActionSetField(
+            #         eth_src=self.topo.dpid_to_mac[dpid][out_port]),
+            #     parser.OFPActionDecNwTtl(),
+            #     parser.OFPActionOutput(out_port)]
+            actions = [
+                parser.OFPActionDecNwTtl(),
+                parser.OFPActionOutput(out_port)]
             match = parser.OFPMatch()
             match.set_dl_type(ether.ETH_TYPE_IP)
             match.set_ip_proto(inet.IPPROTO_ICMP)
-            #match.set_in_port(in_port)
+            # match.set_in_port(in_port)
             # print self.topo.dp_to_mac
-            #match.set_dl_src(mac.haddr_to_bin(src))
+            # match.set_dl_src(mac.haddr_to_bin(src))
             match.set_dl_dst(mac.haddr_to_bin(dst))
             # print(dst, src)
 
@@ -228,7 +239,7 @@ class SimpleSwitch13(app_manager.RyuApp):
             else:
                 self.add_flow(datapath, 65535, match, actions)
         else:
-            print ">>>>FLOOD!"
+            print('>>>>FLOOD!')
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
@@ -242,7 +253,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         minimum = float('inf')
         chosen_node = None
         for switch, value in cost.items():
-            if visited[switch] == False and value < minimum:
+            if visited[switch] is False and value < minimum:
                 minimum = value
                 chosen_node = switch
         return chosen_node
@@ -282,7 +293,10 @@ class SimpleSwitch13(app_manager.RyuApp):
                     cost[neighbor] = 1
                     previous[neighbor] = initial
                 else:
-                    soma = cost[actual] + 1 # Se fosse com pesos diferentes, teria que somar com o peso do link de actual para neighbor
+                    soma = cost[actual] + 1
+                    # Se fosse com pesos diferentes,
+                    # teria que somar com o peso do link
+                    # de actual para neighbor
                     if soma < cost[neighbor]:
                         cost[neighbor] = soma
                         previous[neighbor] = actual
@@ -293,19 +307,19 @@ class SimpleSwitch13(app_manager.RyuApp):
     @classmethod
     def getPaths(cls, initial, switches, previous):
         paths = []
-        #print "\nInitial {}".format(initial)
+        # print('\nInitial {}'.format(initial))
         for dpid in switches:
-            #print "Last {}".format(dpid)
+            # print('Last {}'.format(dpid))
             path = []
             if dpid != initial:
                 hop = previous[dpid]
                 while hop != initial:
-                    #print ">>HOP",hop
+                    # print('>>HOP',hop)
                     path.insert(0, hop)
                     hop = previous[hop]
                 path.insert(0, initial)
                 path.append(dpid)
-                #print path
+                # print path
                 paths.append(path)
         return paths
 
@@ -314,10 +328,10 @@ class SimpleSwitch13(app_manager.RyuApp):
         next_hop = None
         for path in paths:
             if path[0] == dpid_src:
-                if path[-1] == dpid_dst: #get last
+                if path[-1] == dpid_dst:  # get last
                     next_hop = path[1]
-                    print ">>SHOULD FORWARD TO SWITCH:",next_hop
+                    print('>>SHOULD FORWARD TO SWITCH:', next_hop)
         for enlace in enlaces:
             if enlace.dpid_src == dpid_src and enlace.dpid_dst == next_hop:
-                print ">>MATCHING LINK ", str(enlace)
+                print('>>MATCHING LINK ', str(enlace))
                 return enlace.src_port
